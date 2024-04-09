75d58485ee7050385c51ebf5d6754ada9d34926a
Wang Jinghan <53046658+Jinghan-Wang@users.noreply.github.com>
2024-04-09 10:40:21 +0800

    Create 0077_combine.cpp

---
+++ 0077_combine.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int n, int k, int startindex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startindex; i <= n; i++) {
+            path.push_back(i);
+            backtracking(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k){
+        backtracking(n, k, 1);
+        return result;
+    }
+};
+
+
+int main(){
+    Solution solution;
+    int n;
+    int k;
+    cin >> n;
+    cin >> k;
+    vector<vector<int>> result = solution.combine(n, k);
+    for (const auto& row : result) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+}
